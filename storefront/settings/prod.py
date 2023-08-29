0c0cc1747cf95033fc5991c508a4d36a925d1382
Lakshitha <you@example.com>
2023-08-29 15:56:25 +0530

    change the db settings

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -7,20 +7,20 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 ALLOWED_HOSTS = ["co-shop.azurewebsites.net"]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-conn_str = os.environ["AZURE_MYSQL_CONNECTIONSTRING"]
-conn_str_params = {
-    pair.split("=")[0]: pair.split("=")[1] for pair in conn_str.split(" ")
-}
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": conn_str_params["dbname"],
-        "HOST": conn_str_params["host"],
-        "USER": conn_str_params["user"],
-        "PASSWORD": conn_str_params["password"],
+        "NAME": os.environ["AZURE_MYSQL_NAME"],
+        "HOST": os.environ["AZURE_MYSQL_HOST"],
+        "USER": os.environ["AZURE_MYSQL_USER"],
+        "PASSWORD": os.environ["AZURE_MYSQL_PASSWORD"],
     }
 }
