d6ed5f85b9bdbf6ed9dd07366b98e9a4141f8e89
Lakshitha <you@example.com>
2023-08-29 18:29:59 +0530

    edit allowed hosts in production settings

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -5,7 +5,15 @@ DEBUG = False
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-ALLOWED_HOSTS = ["co-shop.azurewebsites.net"]
+ALLOWED_HOSTS = (
+    [os.environ["WEBSITE_HOSTNAME"]] if "WEBSITE_HOSTNAME" in os.environ else []
+)
+CSRF_TRUSTED_ORIGINS = (
+    ["https://" + os.environ["WEBSITE_HOSTNAME"]]
+    if "WEBSITE_HOSTNAME" in os.environ
+    else []
+)
+
 
 STORAGES = {
     "staticfiles": {
